3e12877709a444e81b891bad4b1ac0e324177bb9
Nazar <nazarvasilciv@gmail.com>
2024-03-16 22:08:33 +0100

    Graphics Update

--- Snake.rkt
+++ Snake.rkt
@@ -6,22 +6,23 @@
 
 
 
-;Snake is one of:
+;snake is one of:
 ; - '()
-; - (cons Snake-part Snake)
-(define Snake (list '()))
+; - (cons snakeP Snake)
+(define snake (list '()))
 
 
-;Structure Snake-part represents the individual part of the snake
-;Snake-part consists of coordinates(x y), direction
+;Structure snakeP represents the individual part of the snake
+;snakeP consists of coordinates(x y), direction
 
-;Direction is one of:
+;dir represents the direction of individual snake part
+;dir is one of:
 ; - Up
 ; - Right
 ; - Down
 ; - Left
-(define-struct Snake-part [x y direction])
-(make-Snake-part  20 20 "Right")
+(define-struct snakeP [x y dir])
+(make-snakeP  20 20 "Right")
 
 
 ;Structure Apple represents the apple, if snake touches apple it grows by one Snake-part
@@ -31,7 +32,7 @@
 
 
 
-;Structure Game-State represents the current state of the game
+;Structure WorldState represents the current state of the game
 ;It consists of score, state
 
 ;Score is the amount of points player has gained
@@ -42,18 +43,38 @@
 ; - Before-game
 ; - Game
 ; - Game-over
-(define-struct GameState [score state])
-(make-GameState  0 "Before-game")
+(define-struct WorldState [snake score])
+(make-WorldState (cons (make-snakeP 5 5 "Right") (cons (make-snakeP 30 4 "Left") '())) 900)
+
+
+
+;WS -> Image
+;Will draw everything for the game
+(define (ws-draw WS)
+  (draw-snake (WorldState-snake WS))) 
+
+
+
+;Snake -> Image
+;Will draw every element of the snake
+(define (draw-snake snake)
+  (cond [(empty? snake) (rectangle 1000 1000 "solid" "white")]
+        [(cons? snake)
+          (place-image (rectangle 20 20 "solid" "green") (snakeP-x (first snake)) (snakeP-y (first snake))
+                 (draw-snake (rest snake)))]))
+
+
+
+;WorldState -> WorldState
+;Will change the WorldState every tick
+(define (change-ws WS)
+  (cond
+    [(string=? (snakeP-dir (first (WorldState-snake WS))) "Right")(make-WorldState (cons (make-snakeP (+ 20 (snakeP-x (first (WorldState-snake WS)))) (snakeP-y (first (WorldState-snake WS))) (snakeP-dir (first (WorldState-snake WS)))) (cons (make-snakeP (+ 20 (snakeP-x (second (WorldState-snake WS)))) (snakeP-y (second (WorldState-snake WS))) (snakeP-dir (second (WorldState-snake WS)))) '())) (WorldState-score WS))]))
 
 
 
 
-
-
-
-(cons (make-Snake-part  20 20 "Right") Snake)
-
-
-
-
-
+;The main Handler
+(big-bang (make-WorldState (cons (make-snakeP 500 500 "Right")(cons (make-snakeP 440 500 "Right") '())) 0)
+          [on-tick change-ws 0.25]
+          [to-draw ws-draw])
--- g.rkt
+++ g.rkt
@@ -1,20 +1,43 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname g) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname g) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 (require 2htdp/universe)
 
 
-(define (update ws)
-  (not ws))
+
+(define (create-UFO-scene WS)
+  (draw-snake (WorldState-snake WS))) 
+ 
+;(define UFO
+;  (underlay/align "center"
+;                  "top"
+  ;                (circle 10 "solid" "green"))
+                  ;(rectangle 40 4 "solid" "green"));)
+ (define-struct Apple [x y])
+(make-Apple  20 20)
 
 
-(define (render ws)
-  (if ws
-      (triangle 135 "solid" "red")
-      (square 135 "solid" "blue")))
+(define (draw-snake snake)
+  (cond [(empty? snake) (rectangle 1000 1000 "solid" "white")]
+        [(cons? snake)
+          (place-image (rectangle 20 20 "solid" "green") (snakeP-x (first snake)) (snakeP-y (first snake))
+                 (draw-snake (rest snake)))]))
+  ;(rectangle (Apple-x Apple) (Apple-y Apple) "solid" "green"))
 
 
-(big-bang #false
-  [on-tick update]
-  [on-draw render])
+(define-struct snakeP [x y dir])
+(define-struct WorldState [snake score])
+
+
+
+
+
+(define (change-ws WS)
+  (cond
+    [(string=? (snakeP-dir (first (WorldState-snake WS))) "Right")(make-WorldState (cons (make-snakeP (+ 20 (snakeP-x (first (WorldState-snake WS)))) (snakeP-y (first (WorldState-snake WS))) (snakeP-dir (first (WorldState-snake WS)))) (cons (make-snakeP (+ 20 (snakeP-x (second (WorldState-snake WS)))) (snakeP-y (second (WorldState-snake WS))) (snakeP-dir (second (WorldState-snake WS)))) '())) (WorldState-score WS))]))
+  
+ 
+(big-bang (make-WorldState (cons (make-snakeP 500 500 "Right")(cons (make-snakeP 478 500 "Right") '())) 0)
+          [on-tick change-ws 0.25]
+          [to-draw create-UFO-scene])
